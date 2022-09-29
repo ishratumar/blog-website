3f4c4d27d7dcc3cc2ba422c3291a68bd9c1d5062
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2022-09-29 14:58:13 +0500

    added main container

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
 import Header from "../../header/Header"
+import Posts from "../../posts/Posts"
+import Sidebar from "../../sidebar/Sidebar"
 import "./home.css"
 
 export default function Home() {
   return (
+    <>
+     <Header />
     <div className="home">
-      <Header />
-      homepage
+      <Posts />
+      <Sidebar />
     </div>
+    </>
   )
 }
