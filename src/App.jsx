03a03617aed526c255c4d38264db2642b34ec180
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2022-10-03 11:41:21 +0500

    added register page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import TopBar from "./components/topbar/TopBar"
 import Write from './pages/write/Write';
 import Settings from './pages/settings/Settings';
 import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 export default function App(){
     return(
         <>
         <TopBar />
-        <Login /> 
+        <Register /> 
         </>
     );
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,19 @@
+import "./register.css"
+
+export default function Register() {
+  return (
+    <div className="register">
+        <span className="register-title">Register</span>
+      <form className="register-form">
+        <label>Username</label>
+        <input type="text" placeholder="Enter your username..." className="register-input" />
+        <label>Email</label>
+        <input type="email" placeholder="Enter your email..." className="register-input" />
+        <label>Password</label>
+        <input type="password" placeholder="Enter your password..." className="register-input" />
+        <button className="register-button">Register</button>
+      </form>
+      <button className="register-login-button">Login</button>
+    </div>
+  )
+}
