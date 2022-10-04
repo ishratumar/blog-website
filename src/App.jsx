69e9e0e3d8d47f3a1cdaee52f60a94a95b2ce696
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2022-10-04 13:15:27 +0500

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home"
 import TopBar from "./components/topbar/TopBar"
 import Write from './pages/write/Write';
@@ -7,10 +8,18 @@ import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 
 export default function App(){
-    return(
-        <>
-        <TopBar />
-        <Register /> 
-        </>
-    );
-}
+    const user = false;
+        return (
+            <BrowserRouter>
+            <TopBar />
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/register" element={user ? <Home /> : <Register />} />
+              <Route path="/login" element={user ? <Home /> : <Login />} />
+              <Route path="/settings" element={user ? <Settings /> : <Register />} />
+              <Route path="/write" element={user ? <Write /> : <Register />} />
+              <Route path="/post/:postId" element={<Single />} />
+            </Routes>
+            </BrowserRouter>
+        );
+      }
