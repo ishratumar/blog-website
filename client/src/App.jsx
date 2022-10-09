49f90fb922b616c97eb553700cce72c872215f73
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2022-10-09 13:50:37 +0500

    added api folder

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/home/Home"
+import TopBar from "./components/topbar/TopBar"
+import Write from './pages/write/Write';
+import Settings from './pages/settings/Settings';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Single from './pages/single/Single';
+
+ function App(){
+    const user = true;
+        return (
+            <>
+            <TopBar />
+            <Routes>
+              <Route exact path="/" element={<Home/>} />
+              <Route path="/register" element={user ? <Home /> : <Register />} />
+              <Route path="/login" element={user ? <Home /> : <Login />} />
+              <Route path="/settings" element={user ? <Settings /> : <Register />} />
+              <Route path="/write" element={user ? <Write /> : <Register />} />
+              <Route path="/post/:postId" element={<Single />} />
+            </Routes>
+            </>
+        );
+      }
+      export default App;
